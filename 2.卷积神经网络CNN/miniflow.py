da9c189b711fb7f79a2dca64ef42b1e5cd8a4571
jason yu <yesterdayfinder@gmail.com>
2017-06-20 06:31:16 +0800

    13.随机梯度下降

--- 2.卷积神经网络CNN/miniflow.py
+++ 2.卷积神经网络CNN/miniflow.py
@@ -323,3 +323,17 @@ def forward_and_backward(graph):
     # see: https://docs.python.org/2.3/whatsnew/section-slices.html
     for n in graph[::-1]:
         n.backward()
+
+
+def sgd_update(trainables, learning_rate=1e-2):
+    """
+    Updates the value of each trainable with SGD.
+
+    Arguments:
+
+        `trainables`: A list of `Input` Nodes representing weights/biases.
+        `learning_rate`: The learning rate.
+    """
+    for t in trainables:
+      t.value -= learning_rate * t.gradients[t]
+    
